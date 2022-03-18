ea24883ad8b02977d67dc9ddc81c9a1a8a226598
Saurbh Sagar <saurabh.sagar@e2enetworks.com>
2022-03-18 20:22:10 +0530

    lead_list view added

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Lead
 
 # Create your views here.
+
+
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+    "leads": leads
+    }
+    return render(request, "leads/homepage.html", context)
