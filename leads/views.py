4a947aead14e57818f3a9749b3c8702724875ca5
saurabh sagar <saurabhsagar68@gmail.com>
2022-03-19 15:09:31 +0530

    lead create from created

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Lead
+from .forms import LeadForm
 
 # Create your views here.
 
@@ -7,6 +8,21 @@ from .models import Lead
 def lead_list(request):
     leads = Lead.objects.all()
     context = {
-    "leads": leads
+        "leads": leads
     }
-    return render(request, "leads/homepage.html", context)
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, "leads/lead_detail.html", context)
+
+
+def lead_create(request):
+    context = {
+        "form": LeadForm()
+    }
+    return render(request, 'leads/lead_create.html', context)
