85d131ea08d55d352ca0280e485a24f86d852cae
Saurabh's Mac <saurabhs@Saurabhss-MacBook-Pro.local>
2022-06-16 22:50:28 +0530

    function-based view for creating lead

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Lead
+from django.shortcuts import render, redirect
+from .models import Lead, Agent
 from .forms import LeadForm
 
 # Create your views here.
@@ -22,7 +22,27 @@ def lead_detail(request, pk):
 
 
 def lead_create(request):
+    print(request.method)
+    form = LeadForm()
+    if request.method=="POST":
+        print("a POST request")
+        form = LeadForm(request.POST)
+        # this will again fill whatever values we have passed in each field otherwise passed LeadForm() in
+        # context then fileds would not retain their values
+        if form.is_valid():
+            print("form is validated")
+            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(first_name=first_name,
+                                last_name=last_name,
+                                age=age,
+                                agent=agent)
+            print("lead has been created")
+            return redirect("/leads")
     context = {
-        "form": LeadForm()
+        "form": form
     }
     return render(request, 'leads/lead_create.html', context)
