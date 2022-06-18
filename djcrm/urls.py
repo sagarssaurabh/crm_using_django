b1a886a08664dfdc799c8e8f100a8b8f581d5f57
Saurabh's Mac <saurabhs@Saurabhss-MacBook-Pro.local>
2022-06-18 12:25:23 +0530

    update and delete view created, then dynamic url path in templated added, and implimented 'extend templates' from base.html file

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from leads.views import landing_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_view, name='landing'),
     path('leads/', include("leads.urls", namespace="leads")),
 ]
--- leads/views.py
+++ leads/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
 
-# Create your views here.
 
+# Create your views here.
+def landing_view(request):
+    return render(request, "landing.html")
 
 def lead_list(request):
     leads = Lead.objects.all()
@@ -25,18 +27,10 @@ def lead_create(request):
     print(request.method)
     form = LeadModelForm()
     if request.method=="POST":
-        print("a POST request")
         form = LeadModelForm(request.POST)
         # this will again fill whatever values we have passed in each field otherwise passed LeadForm() in
         # context then fileds would not retain their values
         if form.is_valid():
-            print("form is validated")
-            print(form.cleaned_data)
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = form.cleaned_data['agent']
-            print("lead has been created")
             form.save()
             return redirect("/leads")
     context = {
@@ -44,14 +38,60 @@ def lead_create(request):
     }
     return render(request, 'leads/lead_create.html', context)
 
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method=="POST":
+        form = LeadModelForm(request.POST,instance=lead)
+        if form.is_valid():
+            print("lead has been updated")
+            form.save()
+            return redirect("/leads")
+    context = {
+        "form": form,
+        "lead": lead
+    }
+    return render(request, 'leads/lead_update.html', context)
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
+
+# def lead_update(request, pk):
+#     lead = Lead.objects.get(id=pk)
+#     form = LeadForm(instance=lead)
+#     if request.method=="POST":
+#         print("a POST request")
+#         form = LeadForm(request.POST,instance=lead)
+#         # this will again fill whatever values we have passed in each field otherwise passed LeadForm() in
+#         # context then fileds would not retain their values
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.age = age
+#             print("lead has been updated")
+#             lead.save()
+#             return redirect("/leads")
+#     context = {
+#         "form": form,
+#         "lead": lead
+#     }
+#     return render(request, 'leads/lead_update.html', context)
+
+
+
 
 
     # def lead_create(request):
     #     print(request.method)
-    #     form = LeadModelForm()
+    #     form = LeadForm()
     #     if request.method=="POST":
     #         print("a POST request")
-    #         form = LeadModelForm(request.POST)
+    #         form = LeadForm(request.POST)
     #         # this will again fill whatever values we have passed in each field otherwise passed LeadForm() in
     #         # context then fileds would not retain their values
     #         if form.is_valid():
