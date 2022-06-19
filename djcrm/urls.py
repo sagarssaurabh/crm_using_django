cf399c134c7a080f1f34f799c4320e0487adee58
Saurabh's Mac <saurabhs@Saurabhss-MacBook-Pro.local>
2022-06-19 09:48:46 +0530

    commit before authentication.registration folder created in base template folder is part of authentication

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import landing_view, LandingPageView
@@ -6,4 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing'),
     path('leads/', include("leads.urls", namespace="leads")),
+    # static(settings.STATIC_URL,documnet_root=settings.STATIC_ROOT)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,documnet_root=settings.STATIC_ROOT)
