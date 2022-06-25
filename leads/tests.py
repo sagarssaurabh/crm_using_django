e3d4df282ae8413e5170626003f313fb69b36eab
Saurabh's Mac <saurabhs@Saurabhss-MacBook-Pro.local>
2022-06-25 16:40:27 +0530

    written test cases in test.py file

--- leads/tests.py
+++ leads/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
 # Create your tests here.
+class LandingPageTest(TestCase):
+
+    def test_status_code(self):
+        response = self.client.get(reverse("landing"))
+        # print(dir(response))
+        # print('++++++++++++++++++')
+        # print(response.content)
+        # print('++++++++++++++++++')
+        # print(response.status_code)
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name(self):
+        response = self.client.get(reverse("landing"))
+        self.assertTemplateUsed(response, 'landin.html')
